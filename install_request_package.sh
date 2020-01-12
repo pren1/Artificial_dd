c1a5657d103f442c9345142bbe47a91efcebf633
pren1 <33309499+pren1@users.noreply.github.com>
2020-01-12 13:55:36 -0800

    Run this to get requested packages

---
+++ install_request_package.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+sudo apt update
+sudo apt upgrade
+sudo apt install python3-pip
+pip3 install flask
+pip3 install flask-cors
+pip3 install tensorflow==1.14.0
+pip3 install jieba
+pip3 install gdown
+pip3 install pandas
+pip3 install scipy
+pip3 install tqdm
+pip3 install uwsgi
